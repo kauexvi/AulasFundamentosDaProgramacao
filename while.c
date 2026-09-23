1456c986a5c1f6577be11c1424582892369a39e8
kauexvi <kaueterada@gmail.com>
2026-09-23 11:11:22 -0300

    Learning while and flag!

---
+++ while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+//                                                      tabuada
+
+/*
+int main()
+{
+    int num, i;
+    i = 1;
+    printf("informe numero para gerar a tabuada : ");
+    scanf("%d", &num);
+
+    while (i <= 10)
+    {
+        printf("%d x %d = %d\n", i, num, i * num);
+        i++;
+    }
+}
+*/
